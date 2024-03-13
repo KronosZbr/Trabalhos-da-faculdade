303fa9ac1f5b48a5855363085610a86f000df242
João Vitor Dysarz <dysarzjoaovitor@gmail.com>
2024-03-13 20:53:14 -0300

    teste

--- Semestre 03/Desenvolvimento de software como serviço/apiteste/src/main/java/com/apitest/apiteste/controller/clienteController.java
+++ Semestre 03/Desenvolvimento de software como serviço/apiteste/src/main/java/com/apitest/apiteste/controller/clienteController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apitest.apiteste.model.Cliente;
+
 @RestController
 @RequestMapping("/clientes")
 public class clienteController {
